801d925b44fab7c6c0b3101ef7196924e40af30b
cfsnook <cfs@ecs.soton.ac.uk>
2018-04-26 16:46:44 +0100

    make initalValue text property field RodinKeyboard

--- ac.soton.eventb.classdiagrams.diagram/src/ac/soton/eventb/classdiagrams/diagram/sheet/custom/InitialValuePropertySection.java
+++ ac.soton.eventb.classdiagrams.diagram/src/ac/soton/eventb/classdiagrams/diagram/sheet/custom/InitialValuePropertySection.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2010 University of Southampton.
+ * Copyright (c) 2010-2018 University of Southampton.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -76,56 +76,8 @@ public class InitialValuePropertySection extends AbstractTextPropertySection {
 				) ;
 	}
 	
-//	///////// FIX problem with selfName text changing to Class name when selected /////
-//	// This problem occurred when clicking in the text box for the Self Name property.
-//	// The text field immediately changed to the Class name and would not reset until
-//	// the property sheet was redrawn (e.g. by selecting a different kind of element
-//	// in the diagram).
-//	
-//	/**
-//	 * @return Returns the new listener.
-//	 */
-//	@Override
-//	protected TextChangeHelper getListener() {
-//		return newlistener;
-//	}
-//	
-//	/**
-//	 * A helper to listen for events that indicate that a text field has been
-//	 * changed.
-//	 */
-//	private TextChangeHelper newlistener = new TextChangeHelper() {
-//		boolean textModified = false;
-//		/**
-//		 * @see org.eclipse.swt.widgets.Listener#handleEvent(org.eclipse.swt.widgets.Event)
-//		 */
-//		public void handleEvent(Event event) {
-//			switch (event.type) {
-//				case SWT.Modify :
-//					textModified = !isNonUserChange();
-//					break;
-//				case SWT.KeyDown :
-//					if (event.character == SWT.CR) {
-//						textChanged((Control)event.widget);
-//					}
-//					break;
-//				case SWT.FocusOut :
-//					textChanged((Control)event.widget);
-//					refresh();							//this refresh added to solve default text changing problem
-//					break;
-//			}
-//		}
-//		
-//		public void textChanged(Control control) {
-//			if (textModified) {
-//				// clear error message
-//				IWorkbenchPart part = PlatformUI.getWorkbench()
-//					.getActiveWorkbenchWindow().getActivePage().getActivePart();
-//				StatusLineUtil.outputErrorMessage(part, StringStatics.BLANK);
-//
-//				setPropertyValue(control);
-//				textModified = false;
-//			}
-//		}		
-//	};
+	protected boolean isRodinKeyboard(){
+		return true;
+	}
+	
 }
