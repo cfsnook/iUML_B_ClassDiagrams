50da950177479696139eccef47c60f31cb69e1e3
Dr Colin Snook <cfs@ecs.soton.ac.uk>
2018-08-28 22:30:43 +0100

    add method refines column to class methods property sheet

--- ac.soton.eventb.classdiagrams.diagram/src/ac/soton/eventb/classdiagrams/diagram/sheet/custom/MethodsPropertySection.java
+++ ac.soton.eventb.classdiagrams.diagram/src/ac/soton/eventb/classdiagrams/diagram/sheet/custom/MethodsPropertySection.java
@@ -8,12 +8,18 @@
 
 package ac.soton.eventb.classdiagrams.diagram.sheet.custom;
 
+import java.util.List;
+
+import org.eclipse.emf.common.util.EList;
+import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.EReference;
 import org.eclipse.emf.ecore.EStructuralFeature;
 import org.eclipse.jface.viewers.IFilter;
 import org.eventb.emf.core.CorePackage;
+import org.eventb.emf.core.EventBElement;
 
 import ac.soton.eventb.classdiagrams.Class;
+import ac.soton.eventb.classdiagrams.Classdiagram;
 import ac.soton.eventb.classdiagrams.ClassdiagramsPackage;
 import ac.soton.eventb.emf.core.extension.coreextension.CoreextensionPackage;
 import ac.soton.eventb.emf.diagrams.sheet.AbstractEditTablePropertySection;
@@ -41,15 +47,16 @@ public class MethodsPropertySection extends AbstractEditTablePropertySection {
 	protected EStructuralFeature getFeatureForCol(final int col) {
 		switch (col) {
 		case 0 : return CoreextensionPackage.eINSTANCE.getEventBLabeled_Label();
-		case 1 : return CoreextensionPackage.eINSTANCE.getEventBEventGroup_Extended();
-		case 2 : return CorePackage.eINSTANCE.getEventBCommented_Comment();
+		case 1 : return CoreextensionPackage.Literals.EVENT_BEVENT_GROUP__REFINES;
+		case 2 : return CoreextensionPackage.Literals.EVENT_BEVENT_GROUP__EXTENDED;
+		case 3 : return CorePackage.eINSTANCE.getEventBCommented_Comment();
 		default : return null;
 		}
 	}
 
 	@Override
 	protected boolean isMulti(final int col){
-		return col==2 ? true : false;
+		return col==3 ? true : false;
 	}
 	
 	
@@ -67,8 +74,9 @@ public class MethodsPropertySection extends AbstractEditTablePropertySection {
 	protected int columnWidth(final int col){
 		switch (col) {
 		case 0 : return 250;	//label field
-		case 1 : return 100;	//extended field
-		case 2 : return 400;	//comment field
+		case 1 : return 100;	//refines field
+		case 2 : return 100;	//extended field
+		case 3 : return 400;	//comment field
 		default : return -1;	//unknown
 		}
 	}
@@ -76,6 +84,17 @@ public class MethodsPropertySection extends AbstractEditTablePropertySection {
 	@Override
 	protected String getButtonLabelText() {
 		return "Method  ";
+	}
+	
+	@Override
+	protected List<?> getPossibleValues(final int col){
+		if (col==1) {
+			Classdiagram cd = (Classdiagram) ((EventBElement) eObject).getContaining(ClassdiagramsPackage.Literals.CLASSDIAGRAM);
+			Classdiagram rcd = cd.getRefines();
+			EList<EObject> possibles = rcd.getAllContained(ClassdiagramsPackage.Literals.CLASS_METHOD, true);
+			return possibles;
+		}else
+			return super.getPossibleValues(col);
 	}
 	
 }
